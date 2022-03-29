e4a0238f2c155cd8dbf0e6348993aa7f235eda96
Stephanie Yanaga <stephanie.yanaga@fishtownanalytics.com>
2022-03-29 00:48:51 +0000

    change yml file materializations

--- models/marts/coredim_customers.sql
+++ models/marts/coredim_customers.sql
@@ -1,27 +1,14 @@
-{{ config (
-    materialized="table"
-)}}
+
 
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial`.jaffle_shop.customers
+   select * from {{ref('stg_customers')}}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial`.jaffle_shop.orders
+   select * from {{ref('stg_orders')}}
 
 ),
 
